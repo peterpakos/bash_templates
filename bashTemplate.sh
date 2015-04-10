e58735dd233d32697b5bfdb1fe56b642583b5fab
Peter Pakos <peter@pakos.pl>
2015-04-10 16:52:36 +0100

    Edited bashTemplate.sh

--- bashTemplate.sh
+++ bashTemplate.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #
 # Template for bash scripts
-# Copyright (C) 2014 Peter Pakos
+#
+# Copyright © 2015 Peter Pakos
 
-### Configuration
+### Variables
+
 _VERSION=0.0
 _DIR="$(cd "$(dirname "${0}")" && pwd)"
 _FILE=$(basename "${0}")
 
 ### Functions
 
-function cleanup()
-{
-	return 0
+cleanup() {
+  return 0
 }
 
-function handle_error()
-{
-	echo -e "\n${@}" 1>&2
-#	cleanup
-    exit 1
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+  cleanup
+  exit 1
 }
 
 ### Runtime
@@ -28,15 +28,12 @@ function handle_error()
 set -o errexit
 set -o errtrace
 set -o nounset
-
-# Bash will remember & return the highest exitcode in a chain of pipes.
-# This way you can catch the error in case script fails in `script | gzip`.
 set -o pipefail
 
-trap 'handle_error "Error in ${_FILE} at line $(( ${LINENO} - 1 )) (exit code ${?})"' ERR
-trap 'handle_error "Received signal SIGHUP"' SIGHUP
-trap 'handle_error "Received signal SIGINT"' SIGINT
-trap 'handle_error "Received signal SIGTERM"' SIGTERM
+trap 'err "Error in ${_FILE} at line $(( ${LINENO} - 1 )) (exit code ${?})"' ERR
+trap 'err "Received signal SIGHUP"' SIGHUP
+trap 'err "Received signal SIGINT"' SIGINT
+trap 'err "Received signal SIGTERM"' SIGTERM
 
 ### Main code
 
